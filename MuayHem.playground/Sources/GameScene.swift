34dc8edb21ab92755ca07a39efd2d90fa6d98f3b
SlickJohnson <liwa.johnson@gmail.com>
2018-03-30 13:02:30 -0700

    Add the SKShapeNode that will represent the player

--- MuayHem.playground/Sources/GameScene.swift
+++ MuayHem.playground/Sources/GameScene.swift
@@ -4,6 +4,8 @@ import SpriteKit
 public class GameScene: SKScene {
   /// The SKShapeNode that represents the ground at the bottom of game screen.
   var ground: SKShapeNode!
+  /// The SKShapeNode that represents the player and responds to touch events.
+  var player: SKShapeNode!
 
   public init(size: CGSize, color: SKColor) {
     super.init(size: size)
@@ -27,10 +29,25 @@ public class GameScene: SKScene {
 private extension GameScene {
   /// Configure the game scene with a ground and player.
   func setupScene() {
+    makeGround()
+    makePlayer()
+  }
+
+  /// Creates and adds the SKShapeNode for the ground.
+  func makeGround() {
     let groundSize = CGSize(width: size.width, height: 40)
     ground = SKShapeNode(rect: CGRect(x: 0, y: 0, width: groundSize.width, height: groundSize.height))
     ground.fillColor = .black
     addChild(ground)
+  }
+
+  /// Creates and adds the SKShapeNode for the player.
+  func makePlayer() {
+    let playerRadius: CGFloat = 20
+    player = SKShapeNode(circleOfRadius: playerRadius)
+    player.position = CGPoint(x: size.width / 2, y: size.height / 2)
+    player.fillColor = .red
+    addChild(player)
   }
 }
 
